a19caeaafc9e36060b5432a9b15e8d7336a92b63
Miles Rausch <miles@milesrausch.com>
2020-11-25 14:18:19 -0600

    Hacking in two new pages

---
+++ components/layout/layout.jsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+
+import styles from './layout.module.css';
+
+export default function Layout({ children, title }) {
+    return (
+        <>
+            <Head>
+                <title>{`${
+                    title ? `${title} - ` : ''
+                }Rausch Family: 2020 Christmas Quiz`}</title>
+
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                {children}
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by Next.js
+                </a>
+            </footer>
+        </>
+    );
+}
---
+++ pages/quiz.js
@@ -0,0 +1,11 @@
+import Layout from '../components/layout/layout';
+
+import styles from '../styles/Quiz.module.css';
+
+export default function Quiz() {
+    return (
+        <Layout>
+            <p>Quiz Page.</p>
+        </Layout>
+    );
+}
